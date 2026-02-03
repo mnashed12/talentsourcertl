1e8b399fc0934a233262aafdeaca48505eaae36d
Mark Nashed <mnashed@simpleciti.com>
2026-02-03 17:57:12 -0500

    gdr

--- backend/talentsource/wsgi.py
+++ backend/talentsource/wsgi.py
@@ -1,16 +1,14 @@
-"""
-WSGI config for talentsource project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
+# add your project directory to the sys.path
+project_home = '/home/SimpleCiti/talentsourcertl/backend'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+# set environment variable to tell django where your settings.py is
+os.environ['DJANGO_SETTINGS_MODULE'] = 'talentsource.settings'
+
+# serve django via WSGI
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'talentsource.settings')
-
 application = get_wsgi_application()
